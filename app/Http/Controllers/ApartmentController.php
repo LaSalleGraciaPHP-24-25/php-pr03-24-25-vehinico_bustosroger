2690e835a6399efa35076f54736c2c1faaa5971f
RogerBustos <Rogerbj22@gmail.com>
2025-02-09 17:45:58 +0100

    little error fixed

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -1,4 +1,12 @@
 <?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Apartment;
+use Laravel\Sanctum\HasApiTokens;
 class ApartmentController extends Controller
 {
     public function index(Request $request)
