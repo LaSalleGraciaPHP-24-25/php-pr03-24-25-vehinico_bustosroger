0f0466aceb36cb5722592bbf96c167e861861ca2
RogerBustos <Rogerbj22@gmail.com>
2025-02-09 17:29:00 +0100

    controllers

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,18 @@ use App\Http\Controllers\ApartmentController;
 use App\Http\Controllers\PlatformController;
 use App\Http\Controllers\UserController;
 
-// Rutas de autenticación
 Route::post('/signin', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Rutas de Apartments (protegidas con autenticación)
 Route::middleware('auth:sanctum')->group(function () {
     Route::resource('apartments', ApartmentController::class);
     Route::post('/apartment-platform/{id}', [ApartmentController::class, 'updatePlatform']);
 });
 
-// Rutas adicionales
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('platforms', PlatformController::class);
+});
+
 Route::get('/apartaments_rented', [ApartmentController::class, 'getByRentedStatus']);
 Route::get('/apartaments_high_price', [ApartmentController::class, 'getByHighPrice']);
