941f123b53fb6bad44737554dc7d8ebfce633339
RogerBustos <Rogerbj22@gmail.com>
2025-02-09 18:31:30 +0100

    platform need 2

--- app/Http/Controllers/PlatformController.php
+++ app/Http/Controllers/PlatformController.php
@@ -29,16 +29,5 @@ class PlatformController extends Controller
         return response()->json($platform, 201);
     }
 
-    public function update(Request $request, $id)
-    {
-        $platform = Platform::findOrFail($id);
-        $platform->update($request->all());
-        return response()->json($platform);
-    }
-
-    public function destroy($id)
-    {
-        Platform::findOrFail($id)->delete();
-        return response()->json(['message' => 'Platform deleted']);
-    }
+    
 }
